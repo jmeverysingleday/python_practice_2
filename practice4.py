3ff4218a4b64b07a01be437a72ff5b8d9d3ee2c8
jungmin <nurang0207@yonsei.ac.kr>
2022-05-05 16:47:38 +0900

    python practice 2

--- practice4.py
+++ practice4.py
@@ -10,14 +10,14 @@ sender_email = "nurang99@naver.com"
 receiver_email = "ksjoon28@naver.com"
 
 def sendEmail(addr):
-		reg = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$"
+	reg = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$"
 
-		# bool 함수 -> 내용이 있다면(X none) TRUE, 없다면(none) FALSE
-		if bool(re.match(reg,addr)):
-				smtp.send_message(message)
-				print("정상적으로 메일이 발송되었습니다.")
-		else:
-				print("유효한 이메일 주소가 아닙니다.")
+	# bool 함수 -> 내용이 있다면(X none) TRUE, 없다면(none) FALSE
+	if bool(re.match(reg,addr)):
+			smtp.send_message(message)
+			print("정상적으로 메일이 발송되었습니다.")
+	else:
+			print("유효한 이메일 주소가 아닙니다.")
 
 message = EmailMessage()
 message.set_content("김정민 practice4 이메일입니다.")
@@ -27,7 +27,7 @@ message["From"] = sender_email
 message["To"] = receiver_email
 
 with open("image.png", "rb") as image:
-		image_file = image.read()
+	image_file = image.read()
 
 image_type = imghdr.what("image", image_file)
 message.add_attachment(image_file, maintype = 'image', subtype = image_type)
